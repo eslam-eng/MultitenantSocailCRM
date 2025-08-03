e4dc533a7bfcead446dcb9118a8de91a6c46faf6
eslam <elbadry693@gmail.com>
2025-08-03 19:01:01 +0300

    handle roles and permissions crud

--- config/database.php
+++ config/database.php
@@ -87,12 +87,12 @@ return [
             'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            //            'database' => env('TENANT_DB_USERNAME', 'root'),
-            //            'username' => env('TENANT_DB_USERNAME', 'root'),
-            //            'password' => env('TENANT_DB_USERNAME', ''),
-            'database' => null,
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('TENANT_DB_USERNAME', 'root'),
+            'username' => env('TENANT_DB_USERNAME', 'root'),
+            'password' => env('TENANT_DB_USERNAME', ''),
+//            'database' => null,
+//            'username' => env('DB_USERNAME', 'root'),
+//            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
@@ -190,7 +190,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
+            'prefix' => env('REDIS_PREFIX', Str::slug((string)env('APP_NAME', 'laravel')) . '-database-'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
